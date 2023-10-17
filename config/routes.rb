c1183b41bd5c75271920e95ba5ef85879b01850c
EC2 Default User <ec2-user@ip-172-31-9-180.ap-northeast-1.compute.internal>
2023-10-17 14:24:32 +0000

    kadai2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/new'
-  get 'books/edit'
-  get 'books/show'
+  
+  resources :books
   root 'homes#top'
   
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  get '/top' => 'homes#top'
 end
